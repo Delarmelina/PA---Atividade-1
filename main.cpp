c5b2dc69c442bd9f0e8368bb10af7276b2be3a98
Felipe Ferreira <>
2021-04-16 23:56:01 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+
+  char palavra[50];
+  char aux;
+  const char *teclado[8][4] = { {"a", "b","c", "."}, {"d", "e","f", "."}, {"g", "h","i", "."}, {"j", "k","l", "."}, {"m", "n","o", "."}, {"p", "q","r", "s"}, {"t", "u","v","."}, {"w", "x","y", "z"}};
+
+  cout << "Digite a palavra que quer escrever: ";
+  cin >> palavra;
+
+  for(int L=0 ; L < strlen(palavra) ; L++){
+    for(int i=0;i < 8;i++){
+      for(int j=0;j < 4;j++){
+        if(palavra[L] == *teclado[i][j]){
+          cout << "#" << i+2 << "=" << j+1 << endl;
+  
+        }      
+      }
+    }
+  }
+}
